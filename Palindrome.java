de2ce8ce897cc7f4b78ff27075cfaba0c3159921
Abhhhinav <107941084+Abhhhinav@users.noreply.github.com>
2023-06-05 11:21:45 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,90 @@
+package LinkedList;
+
+public class Palindrome {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public boolean CheckPal() {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        // 1st step
+        Node midNode = findMid(head);
+
+        // 2nd step
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev;
+        Node left = head;
+
+        // 3rd step
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Palindrome ll = new Palindrome();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.display();
+        System.out.println(ll.CheckPal());
+
+    }
+
+}
