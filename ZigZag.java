b374311bbca603b38b9b8ea592faa9b49131dff6
Abhhhinav <107941084+Abhhhinav@users.noreply.github.com>
2023-06-07 12:33:18 +0530

    Add files via upload

---
+++ ZigZag.java
@@ -0,0 +1,87 @@
+package LinkedList;
+
+public class ZigZag {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void Mid() {
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node left = head;
+        Node right = prev;
+        Node NextL, NextR;
+        while (left != null && right != null) {
+            NextL = left.next;
+            left.next = right;
+
+            NextR = right.next;
+            right.next = NextL;
+
+            left = NextL;
+            right = NextR;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ZigZag ll = new ZigZag();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.display();
+        ll.Mid();
+        ll.display();
+
+    }
+
+}
