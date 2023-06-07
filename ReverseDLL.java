d8edca8e7938d71bceb0d0afd65a5c5c5cbdb155
Abhhhinav <107941084+Abhhhinav@users.noreply.github.com>
2023-06-07 18:03:57 +0530

    Add files via upload

---
+++ ReverseDLL.java
@@ -0,0 +1,65 @@
+package LinkedList;
+
+public class ReverseDLL {
+    public static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void Add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+        head.prev = newNode;
+    }
+
+    public void reverse() {
+        Node curr = head;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+
+    }
+
+    public static void main(String[] args) {
+        ReverseDLL dl = new ReverseDLL();
+        dl.Add(4);
+        dl.Add(3);
+        dl.Add(2);
+        dl.Add(1);
+        dl.display();
+        dl.reverse();
+        dl.display();
+    }
+}
